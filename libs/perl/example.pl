91a335b4f3b753db87cfe0e8ff63c2d45f887bf6
Terry Burton <tez@terryburton.co.uk>
2015-09-06 01:28:36 +0100

    Update example.pl

--- libs/perl/example.pl
+++ libs/perl/example.pl
@@ -4,6 +4,6 @@ use strict;
 
 use postscriptbarcode;
 
-$c=new postscriptbarcode::BWIPP("../barcode.ps");
+$c=new postscriptbarcode::BWIPP("../../build/monolithic_package/barcode.ps");
 print $c->get_version();
 
