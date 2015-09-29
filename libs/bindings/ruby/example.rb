eacded87507f01a9647d4cb7fca9e7dc284303f8
Terry Burton <tez@terryburton.co.uk>
2015-09-30 00:23:02 +0100

    Libs: Testing targets.

--- libs/bindings/ruby/example.rb
+++ libs/bindings/ruby/example.rb
@@ -2,7 +2,12 @@
 
 require 'postscriptbarcode'
 
-c=Postscriptbarcode::BWIPP.new(File.dirname(__FILE__)+"/../../../build/monolithic_package/barcode.ps")
-print "Version: " + c.get_version() + "\n"
-print c.emit_all_resources()
+bwipp1=Postscriptbarcode::BWIPP.new(File.dirname(__FILE__)+"/../../../build/monolithic_package/barcode.ps")
+bwipp2=Postscriptbarcode::BWIPP.new(File.dirname(__FILE__)+"/../../../build/monolithic/barcode.ps")
+
+puts "Packaged version: " + bwipp1.get_version()
+puts "Unpackaged version: " + bwipp2.get_version()
+
+puts "Packaged lines: " + bwipp1.emit_all_resources().lines.count.to_s
+puts "Unpackaged lines: " + bwipp2.emit_all_resources().lines.count.to_s
 
