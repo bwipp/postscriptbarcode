3ac024a7527fc4679fb4ae7cd1db51ef81247538
Terry Burton <tez@terryburton.co.uk>
2026-01-31 14:38:56 +0000

    Satiate linter

--- contrib/development/make_image.sh
+++ contrib/development/make_image.sh
@@ -27,33 +27,33 @@ SCALEY=2
 ROTATE=0
 
 while [[ "$1" == --* ]]; do
-    case "$1" in
-        --crop)
-            CROP=true
-            shift
-            ;;
-        --scale=*)
-            SCALEX="${1#*=}"
-            SCALEY="$SCALEX"
-            shift
-            ;;
-        --scalex=*)
-            SCALEX="${1#*=}"
-            shift
-            ;;
-        --scaley=*)
-            SCALEY="${1#*=}"
-            shift
-            ;;
-        --rotate=*)
-            ROTATE="${1#*=}"
-            shift
-            ;;
-        *)
-            echo "Unknown option: $1" >&2
-            exit 1
-            ;;
-    esac
+	case "$1" in
+	--crop)
+		CROP=true
+		shift
+		;;
+	--scale=*)
+		SCALEX="${1#*=}"
+		SCALEY="$SCALEX"
+		shift
+		;;
+	--scalex=*)
+		SCALEX="${1#*=}"
+		shift
+		;;
+	--scaley=*)
+		SCALEY="${1#*=}"
+		shift
+		;;
+	--rotate=*)
+		ROTATE="${1#*=}"
+		shift
+		;;
+	*)
+		echo "Unknown option: $1" >&2
+		exit 1
+		;;
+	esac
 done
 
 FORMAT="$1"
@@ -66,47 +66,47 @@ DATA_HEX=$(printf '%s' "$DATA" | xxd -p | tr -d '\n')
 OPTIONS_HEX=$(printf '%s' "$OPTIONS" | xxd -p | tr -d '\n')
 
 if [ -z "$FORMAT" ] || [ -z "$ENCODER" ] || [ -z "$DATA" ]; then
-    echo "Usage: $0 [options] <format> <encoder> <data> [barcode_options] > output" >&2
-    echo "Options: [--crop] [--scale=N] [--scalex=N] [--scaley=N] [--rotate=N]" >&2
-    echo "Example: $0 png ean13 '1231231231232' 'includetext' > ean13.png" >&2
-    echo "         $0 --scale=2 png qrcode 'test' > qrcode.png" >&2
-    exit 1
+	echo "Usage: $0 [options] <format> <encoder> <data> [barcode_options] > output" >&2
+	echo "Options: [--crop] [--scale=N] [--scalex=N] [--scaley=N] [--rotate=N]" >&2
+	echo "Example: $0 png ean13 '1231231231232' 'includetext' > ean13.png" >&2
+	echo "         $0 --scale=2 png qrcode 'test' > qrcode.png" >&2
+	exit 1
 fi
 
 if [ "$FORMAT" != "png" ] && [ "$FORMAT" != "eps" ]; then
-    echo "Error: format must be 'png' or 'eps'" >&2
-    exit 1
+	echo "Error: format must be 'png' or 'eps'" >&2
+	exit 1
 fi
 
 BWIPP="build/monolithic/barcode.ps"
 OFFSET=3000
 
 if [ ! -f "$BWIPP" ]; then
-    echo "Error: $BWIPP not found. Run 'make' first." >&2
-    exit 1
+	echo "Error: $BWIPP not found. Run 'make' first." >&2
+	exit 1
 fi
 
 if [ "$CROP" = true ]; then
-    INIT="/uk.co.terryburton.bwipp.global_ctx 100 dict def"
-    WHITE_IS_OPAQUE=false
+	INIT="/uk.co.terryburton.bwipp.global_ctx 100 dict def"
+	WHITE_IS_OPAQUE=false
 else
-    INIT="/uk.co.terryburton.bwipp.global_ctx 100 dict def uk.co.terryburton.bwipp.global_ctx /default_backgroundcolor (FFFFFF) put"
-    WHITE_IS_OPAQUE=true
+	INIT="/uk.co.terryburton.bwipp.global_ctx 100 dict def uk.co.terryburton.bwipp.global_ctx /default_backgroundcolor (FFFFFF) put"
+	WHITE_IS_OPAQUE=true
 fi
 
 BBOX=$(gs -q -dNOSAFER -dNOPAUSE -dBATCH -sDEVICE=bbox \
-    -c "<< /PageOffset [$OFFSET $OFFSET] /WhiteIsOpaque $WHITE_IS_OPAQUE >> setpagedevice" \
-    -c "$INIT" \
-    -c "($BWIPP) run $SCALEX $SCALEY scale $ROTATE rotate 0 0 moveto" \
-    -c "<$DATA_HEX> <$OPTIONS_HEX> /$ENCODER /uk.co.terryburton.bwipp findresource exec showpage" \
-    2>&1 | grep "%%BoundingBox:")
+	-c "<< /PageOffset [$OFFSET $OFFSET] /WhiteIsOpaque $WHITE_IS_OPAQUE >> setpagedevice" \
+	-c "$INIT" \
+	-c "($BWIPP) run $SCALEX $SCALEY scale $ROTATE rotate 0 0 moveto" \
+	-c "<$DATA_HEX> <$OPTIONS_HEX> /$ENCODER /uk.co.terryburton.bwipp findresource exec showpage" \
+	2>&1 | grep "%%BoundingBox:")
 
 if [ -z "$BBOX" ]; then
-    echo "Error: Failed to get bounding box" >&2
-    exit 1
+	echo "Error: Failed to get bounding box" >&2
+	exit 1
 fi
 
-read -r _ X1 Y1 X2 Y2 <<< "$BBOX"
+read -r _ X1 Y1 X2 Y2 <<<"$BBOX"
 
 TRANSLATE_X=$((OFFSET - X1))
 TRANSLATE_Y=$((OFFSET - Y1))
@@ -115,19 +115,19 @@ WIDTH=$((X2 - X1))
 HEIGHT=$((Y2 - Y1))
 
 if [ "$FORMAT" = "png" ]; then
-    gs -q -dNOSAFER -dNOPAUSE -dBATCH -sDEVICE=png16m \
-        -dGraphicsAlphaBits=1 -dTextAlphaBits=4 \
-        -g${WIDTH}x${HEIGHT} \
-        -sOutputFile=- \
-        -c "$INIT" \
-        -c "($BWIPP) run $TRANSLATE_X $TRANSLATE_Y translate $SCALEX $SCALEY scale $ROTATE rotate 0 0 moveto" \
-        -c "<$DATA_HEX> <$OPTIONS_HEX> /$ENCODER /uk.co.terryburton.bwipp findresource exec showpage"
+	gs -q -dNOSAFER -dNOPAUSE -dBATCH -sDEVICE=png16m \
+		-dGraphicsAlphaBits=1 -dTextAlphaBits=4 \
+		-g${WIDTH}x${HEIGHT} \
+		-sOutputFile=- \
+		-c "$INIT" \
+		-c "($BWIPP) run $TRANSLATE_X $TRANSLATE_Y translate $SCALEX $SCALEY scale $ROTATE rotate 0 0 moveto" \
+		-c "<$DATA_HEX> <$OPTIONS_HEX> /$ENCODER /uk.co.terryburton.bwipp findresource exec showpage"
 fi
 
 if [ "$FORMAT" = "eps" ]; then
-    gs -q -dNOSAFER -dNOPAUSE -dBATCH -sDEVICE=eps2write \
-        -sOutputFile=- \
-        -c "$INIT" \
-        -c "($BWIPP) run $TRANSLATE_X $TRANSLATE_Y translate $SCALEX $SCALEY scale $ROTATE rotate 0 0 moveto" \
-        -c "<$DATA_HEX> <$OPTIONS_HEX> /$ENCODER /uk.co.terryburton.bwipp findresource exec showpage"
+	gs -q -dNOSAFER -dNOPAUSE -dBATCH -sDEVICE=eps2write \
+		-sOutputFile=- \
+		-c "$INIT" \
+		-c "($BWIPP) run $TRANSLATE_X $TRANSLATE_Y translate $SCALEX $SCALEY scale $ROTATE rotate 0 0 moveto" \
+		-c "<$DATA_HEX> <$OPTIONS_HEX> /$ENCODER /uk.co.terryburton.bwipp findresource exec showpage"
 fi
